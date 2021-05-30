7a101793a5a5cdf88d10ea74177c3d8a1a300c98
tadassa001 <46349655+tadassa001@users.noreply.github.com>
2021-05-30 12:29:37 +0300

    now conected to firestore and when friend addde auto refreshes in the myFreinds screen

---
+++ lib/UserFreindsListScreen.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import 'package:flutter/material.dart';
+
+class UserFreindsListScreen extends StatefulWidget {
+  static const routeName = '/user-friends-list';
+  @override
+  _UserFreindsListScreenState createState() => _UserFreindsListScreenState();
+}
+
+class _UserFreindsListScreenState extends State<UserFreindsListScreen> {
+  final String imageUrl = "gs://bumperapp-9f0e6.appspot.com/";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My friends"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // navigate to add friend screen (don't exist)
+            },
+          ),
+        ],
+      ),
+      // backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: Colors.amber[100],
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection("users")
+              .document("000")
+              .collection("friends")
+              .snapshots(),
+          builder: (
+            BuildContext ctx,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return ListView(
+                children: snapshot.data.documents.map((friend) {
+                  return Center(
+                    child: Card(
+                      shape: StadiumBorder(
+                        side: BorderSide(
+                          color: Colors.black,
+                          width: 2.0,
+                        ),
+                      ),
+                      color: Colors.blue[50],
+                      child: ListTile(
+                        //tileColor: Colors.lightBlue,
+                        leading: CircleAvatar(
+                          // Image.network(FirebaseStorage.instance.ref().child("bless.png").getPath())
+                          // Image.network("gs://bumperapp-9f0e6.appspot.com/")
+                          // backgroundImage: NetworkImage("gs://bumperapp-9f0e6.appspot.com/" + "bless.png")),
+                          backgroundImage: NetworkImage(
+                              "https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg"),
+                        ),
+                        trailing: Text(
+                          "status - dynamic color",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        title: Text(friend["name"]),
+                        onTap: () {},
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+            }
+          }),
+    );
+  }
+}
